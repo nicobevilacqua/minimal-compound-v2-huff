e25667ae655996c8e0da2a9444dbd25cff3554aa
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2023-04-06 19:15:13 -0300

    setup

---
+++ test/Pool.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
+import {IPool} from "src/interfaces/IPool.sol";
+
+contract PoolTest is Test {
+    IPool private pool;
+
+    function setUp() public {
+        pool = IPool(HuffDeployer.config().deploy("Pool"));
+    }
+
+    function test_Pool() external {
+    }
+}
